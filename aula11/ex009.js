89f3753cc2079569d8278bc9e5826763a9f739ae
ce65130@gmail.com <163127840+Carlostupi@users.noreply.github.com>
2024-07-19 17:51:49 -0300

    exercicios 8 e 9

---
+++ aula11/ex009.js
@@ -0,0 +1,8 @@
+var pais = 'CAMBOJA'
+console.log('vivendo em '+ pais)
+if(pais != 'brasil'){
+    console.log('voce é estrangeiro')
+
+}else {
+    console.log('voce é brasileiro') 
+}
---
+++ aula12/ex011.js
@@ -0,0 +1,6 @@
+var idade = 22
+if (idade < 18){
+    console.log('maior de idade')
+}else {
+    console.log('menor de idade')
+}
