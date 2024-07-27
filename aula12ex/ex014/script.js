050e153b7c371bfe66abe57a48a80e363771cf9a
ce65130@gmail.com <163127840+Carlostupi@users.noreply.github.com>
2024-07-26 23:40:02 -0300

    imagens add

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,7 @@
- alert ('ola mundo')
+ function carregar(){  
+    var data = new Date() 
+    var hora = data.getHours()
+    var msg = window.document.getElementById('msg')
+    var foto = window.document.getElementById('imagem')
+    msg.innerHTML = 'agora são '+ hora + 'horas'
+}
