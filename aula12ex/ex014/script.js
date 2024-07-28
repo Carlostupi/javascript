6a4034ab9c2cd953d18a34a191adc2a59c94a52e
ce65130@gmail.com <163127840+Carlostupi@users.noreply.github.com>
2024-07-27 23:41:38 -0300

    opa

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -2,6 +2,16 @@
     var data = new Date() 
     var hora = data.getHours()
     var msg = window.document.getElementById('msg')
-    var foto = window.document.getElementById('imagem')
+    var img = window.document.getElementById('imagem')
     msg.innerHTML = 'agora são '+ hora + 'horas'
-}
+    if (hora >= 0 && hora < 12){
+        img.scr = 'manha.jpg'
+        document.body.style.background = '#FE8E00'
+    }else if (hora >= 12 && hora <= 18){
+        img.scr = 'tarde.jpg'
+        document.body.style.background = '#668299'
+    }else {
+        img.scr = 'noite.jpg'
+        document.body.style.background = '#01366A'
+    }
+}f
