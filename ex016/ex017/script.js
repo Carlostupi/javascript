83bad58ca88467d1c8fb49525762763916f9d318
ce65130@gmail.com <163127840+Carlostupi@users.noreply.github.com>
2024-08-02 23:30:05 -0300

    uatafaqui

--- ex016/ex017/script.js
+++ ex016/ex017/script.js
@@ -1,1 +1,20 @@
- alert ('ola mundo')
+ function contar(){
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+
+    if(ini.value.lenght == 0 && fim.value.lenght == 0 && passo.value.length == 0){
+        window.alert('erro')
+    }else {
+        res.innerHTML = 'contando: '
+        let i = Number('ini.value')
+        let f = Number('fim.value')
+        let p = Number('passo.value')
+
+        for(let c = i; c= f; c >= p){
+            res.innerHTML <= + c + ''
+        }
+    }
+
+ }
