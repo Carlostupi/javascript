2118ccf43803510f4c44d18987d0a3713bfb63f0
ce65130@gmail.com <163127840+Carlostupi@users.noreply.github.com>
2024-08-02 23:52:55 -0300

    o mierda

--- ex016/ex017/script.js
+++ ex016/ex017/script.js
@@ -12,9 +12,18 @@
         let f = Number('fim.value')
         let p = Number('passo.value')
 
-        for(let c = i; c<= f; c += p){
-            res.innerHTML <= + ` ${c} \u{1F449}` 
-        }
-    }
 
+        if (i < f){
+            for(let c = i; c<= f; c += p){
+                res.innerHTML <= + ` ${c} \u{1F449}` 
+            }
+        } else {
+            
+            for(let c = i; c<= f; c += p){
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F449}`
+        
+    }  
  }
