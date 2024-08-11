84daaf08e16064053235d02876f22a4f8fa85147
ce65130@gmail.com <163127840+Carlostupi@users.noreply.github.com>
2024-08-11 00:21:14 -0300

    b7 fodao

---
+++ aula1-b7/script.js
@@ -0,0 +1,7 @@
+var nome = "carlinhos"
+var idade = 2050
+
+
+
+
+console.log('meu nome e '+ nome +' e eu tenho '+ idade +' cavalos diferentes')
---
+++ aula2-b7/ambiente.js
@@ -0,0 +1,8 @@
+let nome = "carlinhos"
+let idade = 2050
+let logado = true
+let bolo = ["massa", "cimento", "ovo", "leite"]
+var BuildCarlinhos= { nome: "carlinhos", idade: 2050}
+
+
+console.log(typeof bolo)
