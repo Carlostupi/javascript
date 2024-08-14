ee25f2d84d65987580154e235f0ca86e6a9b4ad1
ce65130@gmail.com <163127840+Carlostupi@users.noreply.github.com>
2024-08-13 21:45:04 -0300

    switch

--- aula3-b7/script.js
+++ aula3-b7/script.js
@@ -1,5 +1,16 @@
-let age = 177
+let profession = 'milico'
 
-let isAdult = age >= 18 ? 'sim' : 'não';
-
-console.log(isAdult);
+switch (profession){
+    case 'policial':
+      console.log('camisa preta');
+    break;
+    case 'bombina':
+       console.log('sua camisa e vermeia');
+    break ;
+    case 'milico':
+       console.log('sua camisa e verder');
+    break;
+    default:
+       console.log('sua camisxa e branca');
+    break;
+}
