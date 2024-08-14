c3c08e73cba5a3d562a8fb22960e4ce68e050449
ce65130@gmail.com <163127840+Carlostupi@users.noreply.github.com>
2024-08-13 22:00:07 -0300

    funcao

---
+++ aula4-b7/ambiente.js
@@ -0,0 +1,6 @@
+function carlinhos(){
+    console.log('o carlinhos bebeu quatro litros de agua, ')
+    console.log('sendo 2 litros agua potavel e 2 litros agua mineral')
+}
+
+console.log('claide voce me ama')
