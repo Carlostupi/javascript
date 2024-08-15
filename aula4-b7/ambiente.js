7c633e4024d6daaad9d5ae37a6ab76341e199bd8
ce65130@gmail.com <163127840+Carlostupi@users.noreply.github.com>
2024-08-14 21:53:51 -0300

    usar função

--- aula4-b7/ambiente.js
+++ aula4-b7/ambiente.js
@@ -2,5 +2,5 @@ function carlinhos(){
     console.log('o carlinhos bebeu quatro litros de agua, ')
     console.log('sendo 2 litros agua potavel e 2 litros agua mineral')
 }
-
-console.log('claide voce me ama')
+carlinhos ()
+console.log('claide voce me ama');
